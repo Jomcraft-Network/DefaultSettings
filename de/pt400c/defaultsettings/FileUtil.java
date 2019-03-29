b7edcd8779e8f611f296db4f6e9dcd0258d62fbd
PT400C <pt400c@gmail.com>
2019-03-29 18:10:36 +0100

    First 1.5.2 commit

---
+++ de/pt400c/defaultsettings/FileUtil.java
@@ -0,0 +1,247 @@
+package de.pt400c.defaultsettings;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import org.apache.commons.io.FileUtils;
+import cpw.mods.fml.client.FMLClientHandler;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
+
+public class FileUtil {
+	
+	public static final Minecraft MC = Minecraft.getMinecraft();
+	public static final File mcDataDir = MC.mcDataDir;
+	
+	public static File getMainFolder() {
+		final File storeFolder = new File(mcDataDir, "config/defaultsettings");
+		storeFolder.mkdir();
+		return storeFolder;
+	}
+
+	public static void restoreContents() throws NullPointerException, IOException {
+		
+		final File options = new File(mcDataDir, "options.txt");
+		boolean firstBoot = !options.exists();
+		if (firstBoot) {
+			restoreOptions();
+			restoreConfigs();
+		}
+		final File optionsOF = new File(mcDataDir, "optionsof.txt");
+		if (!optionsOF.exists()) 
+			restoreOptionsOF();
+		
+		final File serversFile = new File(mcDataDir, "servers.dat");
+		if (!serversFile.exists()) 
+			restoreServers();
+		
+	}
+	
+	public static void restoreOptions() throws NullPointerException, IOException {
+		final File optionsFile = new File(getMainFolder(), "options.txt");
+		if (optionsFile.exists()) {
+			BufferedReader reader = null;
+			PrintWriter writer = null;
+			try {
+				reader = new BufferedReader(new FileReader(optionsFile));	
+				writer = new PrintWriter(new FileWriter(new File(mcDataDir, "options.txt")));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					writer.print(line + "\n");
+				}
+			} catch (IOException e) {
+				throw e;
+			} finally {
+				try {
+					reader.close();
+					writer.close();
+				} catch (IOException e) {
+					throw e;
+				} catch (NullPointerException e) {
+					throw e;
+				}
+			}
+		}
+	}
+	
+	public static void restoreKeys() throws NullPointerException, IOException, NumberFormatException {
+		DefaultSettings.keyRebinds.clear();
+		final File keysFile = new File(getMainFolder(), "keys.txt");
+		if (keysFile.exists()) {
+			BufferedReader reader = null;
+			try {
+				reader = new BufferedReader(new FileReader(keysFile));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					if (line.isEmpty()) {
+						continue;
+					}
+					DefaultSettings.keyRebinds.put(line.split(":")[0], Integer.parseInt(line.split(":")[1]));
+				}
+			} catch (IOException e) {
+				throw e;
+				
+			} catch (NullPointerException e) {
+				throw e;
+			} finally {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					throw e;
+				} catch (NullPointerException e) {
+					throw e;
+				}
+			}
+
+			for (KeyBinding keyBinding : MC.gameSettings.keyBindings) {
+				if (DefaultSettings.keyRebinds.containsKey(keyBinding.keyDescription)) {
+					keyBinding.keyCode = DefaultSettings.keyRebinds.get(keyBinding.keyDescription);
+				}
+			}
+			
+			KeyBinding.resetKeyBindingArrayAndHash();
+		}
+	}
+	
+	public static void restoreOptionsOF() throws IOException {
+		final File optionsOFFile = new File(getMainFolder(), "optionsof.txt");
+		if (optionsOFFile.exists()) {
+			BufferedReader reader = null;
+			PrintWriter writer = null;
+			try {
+				reader = new BufferedReader(new FileReader(optionsOFFile));
+				writer = new PrintWriter(new FileWriter(new File(mcDataDir, "optionsof.txt")));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					writer.print(line + "\n");
+				}
+			} catch (IOException e) {
+				throw e;
+			} catch (NullPointerException e) {
+				throw e;
+			} finally {
+				try {
+					reader.close();
+					writer.close();
+				} catch (IOException e) {
+					throw e;
+				} catch (NullPointerException e) {
+					throw e;
+				}
+			}
+		}
+	}
+	
+	public static void restoreConfigs() throws IOException {
+		try {
+			FileFilter fileFilter = new FileFilter() {
+				@Override
+				public boolean accept(File file) {
+
+					if (!file.getName().equals("defaultsettings") && !file.getName().equals("keys.txt") && !file.getName().equals("options.txt") && !file.getName().equals("optionsof.txt") && !file.getName().equals("servers.dat"))
+						return true;
+
+					return false;
+				}
+			};
+			FileUtils.copyDirectory(getMainFolder(), new File(mcDataDir, "config"), fileFilter);
+		} catch (IOException e) {
+			throw e;
+		}
+	}
+
+	public static void restoreServers() throws IOException {
+		try {
+			FileUtils.copyFile(new File(getMainFolder(), "servers.dat"), new File(mcDataDir, "servers.dat"));
+		} catch (IOException e) {
+			throw e;
+		}
+	}
+	
+	public static void saveKeys() throws IOException, NullPointerException {
+		PrintWriter writer = null;
+		try {
+			writer = new PrintWriter(new FileWriter(new File(getMainFolder(), "keys.txt")));
+			for (KeyBinding keyBinding : MC.gameSettings.keyBindings) {
+				writer.print(keyBinding.keyDescription + ":" + keyBinding.keyCode + "\n");
+			}
+		} catch (IOException e) {
+			throw e;
+		} catch (NullPointerException e) {
+			throw e;
+		} finally {
+			writer.close();
+		}
+	}
+
+	public static void saveOptions() throws NullPointerException, IOException {
+		MC.gameSettings.saveOptions();
+		PrintWriter writer = null;
+		BufferedReader reader = null;
+		try {
+			writer = new PrintWriter(new FileWriter(new File(getMainFolder(), "options.txt")));
+			reader = new BufferedReader(new FileReader(new File(mcDataDir, "options.txt")));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				if (line.startsWith("key_")) {
+					continue;
+				}
+				writer.print(line + "\n");
+			}
+		} catch (IOException e) {
+			throw e;
+		} catch (NullPointerException e) {
+			throw e;
+		} finally {
+			try {
+				reader.close();
+				writer.close();
+			} catch (IOException e) {
+				throw e;
+			} catch (NullPointerException e) {
+				throw e;
+			}
+		}
+
+		if (!FMLClientHandler.instance().hasOptifine()) {
+			return;
+		}
+
+		try {
+			writer = new PrintWriter(new FileWriter(new File(getMainFolder(), "optionsof.txt")));
+			reader = new BufferedReader(new FileReader(new File(mcDataDir, "optionsof.txt")));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				writer.print(line + "\n");
+			}
+		} catch (IOException e) {
+			throw e;
+		} catch (NullPointerException e) {
+			throw e;
+		} finally {
+			try {
+				reader.close();
+				writer.close();
+			} catch (IOException e) {
+				throw e;
+			} catch (NullPointerException e) {
+				throw e;
+			}
+		}	
+	}
+
+	public static void saveServers() throws IOException {
+		final File serversFile = new File(mcDataDir, "servers.dat");
+		if (serversFile.exists()) {
+			try {
+				FileUtils.copyFile(serversFile, new File(getMainFolder(), "servers.dat"));
+			} catch (IOException e) {
+				throw e;
+			}
+		}
+	}
+}
---
+++ de/pt400c/defaultsettings/GuiConfig.java
@@ -0,0 +1,275 @@
+package de.pt400c.defaultsettings;
+
+import static de.pt400c.defaultsettings.FileUtil.MC;
+
+import java.nio.channels.ClosedByInterruptException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.util.EnumChatFormatting;
+
+public class GuiConfig extends GuiScreen
+{
+    public final GuiScreen parentScreen;
+    public HoverChecker hoverS;
+    public HoverChecker hoverK;
+    public HoverChecker hoverO;
+    public GuiButton buttonS;
+    public GuiButton buttonK;
+    public GuiButton buttonO;
+    private ExecutorService tpe = new ThreadPoolExecutor(1, 3, 30, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+    private ButtonState[] cooldowns = new ButtonState[] {new ButtonState(false, 0), new ButtonState(false, 0), new ButtonState(false, 0)};
+
+    public GuiConfig(GuiScreen parentScreen)
+    {
+        this.mc = MC;
+        this.parentScreen = parentScreen;
+    }
+
+    @Override
+    public void initGui()
+    {
+        Keyboard.enableRepeatEvents(true);
+        this.buttonList.add(this.buttonO = new GuiButton(0, this.width / 2 - 49, this.height / 2 - 20, 98, 20, "Save options"));
+        this.buttonList.add(this.buttonS = new GuiButton(1, this.width / 2 - 149, this.height / 2 - 20, 98, 20, "Save servers"));
+        this.buttonList.add(this.buttonK = new GuiButton(2, this.width / 2 + 51, this.height / 2 - 20, 98, 20, "Save keys"));
+        this.buttonList.add(new GuiButton(3, this.width / 2 - 49, this.height / 2 + 24, 98, 20, "Quit"));
+        this.hoverS = new HoverChecker(this.buttonS, 0);
+        this.hoverK = new HoverChecker(this.buttonK, 0);
+        this.hoverO = new HoverChecker(this.buttonO, 0);
+    }
+
+    @Override
+    public void onGuiClosed() {
+    	Keyboard.enableRepeatEvents(false);
+    	tpe.shutdownNow();
+    }
+
+	@Override
+	protected void actionPerformed(GuiButton button) {
+		if (button.id == 2) {
+			this.cooldowns[2].setProgress(true);
+			tpe.execute(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						saveKeys();
+					} catch (ClosedByInterruptException e) {
+						DefaultSettings.log.log(Level.FINEST, "An exception occurred while saving the key configuration: Interruption exception");
+					}
+				}
+			});
+
+		} else if (button.id == 0) {
+			this.cooldowns[0].setProgress(true);
+			tpe.execute(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						saveOptions();
+					} catch (ClosedByInterruptException e) {
+						DefaultSettings.log.log(Level.FINEST, "An exception occurred while saving the default game options: Interruption exception");
+					}
+				}
+			});
+
+		} else if (button.id == 1) {
+			this.cooldowns[1].setProgress(true);
+			tpe.execute(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						saveServers();
+					} catch (ClosedByInterruptException e) {
+						DefaultSettings.log.log(Level.FINEST, "An exception occurred while saving the server list: Interruption exception");
+					}
+				}
+			});
+		} else if (button.id == 3) {
+			MC.displayGuiScreen(this.parentScreen);
+		}
+	}
+
+    @Override
+    public void updateScreen()
+    {
+    	super.updateScreen();
+        for(int id = 0; id < cooldowns.length; id++) {
+        	if(cooldowns[id].renderCooldown > 0)
+        		cooldowns[id].renderCooldown--;
+        	else if(cooldowns[id].renderCooldown < 0) 
+        		cooldowns[id].renderCooldown++;	
+        }
+    }
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks)
+    {
+    	
+        this.drawDefaultBackground();
+        this.drawGradientRect(10, 10, this.width - 10, this.height - 10, -1072689136, -804253680);
+        this.drawCenteredString(this.fontRenderer, "- DefaultSettings -", this.width / 2, 20, 16777215);
+        this.drawCenteredString(this.fontRenderer, "Control GUI", this.width / 2, 30, 16777215);
+        
+        buttonS.displayString = (cooldowns[1].getProgress() ? EnumChatFormatting.GOLD : cooldowns[1].renderCooldown < 0 ? EnumChatFormatting.RED : cooldowns[1].renderCooldown > 0 ? EnumChatFormatting.GREEN : "") + "Save servers";
+        buttonK.displayString = (cooldowns[2].getProgress() ? EnumChatFormatting.GOLD : cooldowns[2].renderCooldown < 0 ? EnumChatFormatting.RED : cooldowns[2].renderCooldown > 0 ? EnumChatFormatting.GREEN : "") + "Save keys";
+        buttonO.displayString = (cooldowns[0].getProgress() ? EnumChatFormatting.GOLD : cooldowns[0].renderCooldown < 0 ? EnumChatFormatting.RED : cooldowns[0].renderCooldown > 0 ? EnumChatFormatting.GREEN : "") + "Save options";
+        
+        super.drawScreen(mouseX, mouseY, partialTicks);
+
+        if (this.hoverS.checkHover(mouseX, mouseY))
+            this.drawToolTip(MC.fontRenderer.listFormattedStringToWidth("Save your servers", 300), mouseX, mouseY, MC.fontRenderer);
+        if (this.hoverK.checkHover(mouseX, mouseY))
+            this.drawToolTip(MC.fontRenderer.listFormattedStringToWidth("Save keybindings", 300), mouseX, mouseY, MC.fontRenderer);
+        if (this.hoverO.checkHover(mouseX, mouseY))
+            this.drawToolTip(MC.fontRenderer.listFormattedStringToWidth("Save all default game options", 300), mouseX, mouseY, MC.fontRenderer);
+    }
+    
+    public void saveServers() throws ClosedByInterruptException {
+    	try {
+			FileUtil.saveServers();
+			this.cooldowns[1].renderCooldown = 30;
+		} catch (ClosedByInterruptException e){
+			throw e;
+		} catch (Exception e) {
+			DefaultSettings.log.log(Level.SEVERE, "An exception occurred while saving the server list:", e);
+			this.cooldowns[1].renderCooldown = -30;
+		}
+    	this.cooldowns[1].setProgress(false);
+    }
+    
+    public void saveOptions() throws ClosedByInterruptException {
+    	try {
+			FileUtil.saveOptions();
+			this.cooldowns[0].renderCooldown = 30;
+		} catch (ClosedByInterruptException e){
+			throw e;
+		} catch (Exception e) {
+			DefaultSettings.log.log(Level.SEVERE, "An exception occurred while saving the default game options:", e);
+			this.cooldowns[0].renderCooldown = -30;
+		}
+    	this.cooldowns[0].setProgress(false);
+    }
+    
+    public void saveKeys() throws ClosedByInterruptException {
+    	try {
+			FileUtil.saveKeys();
+			this.cooldowns[2].renderCooldown = 30;
+			FileUtil.restoreKeys();
+		} catch (ClosedByInterruptException e){
+			throw e;
+		} catch (Exception e) {
+			DefaultSettings.log.log(Level.SEVERE, "An exception occurred while saving the key configuration:", e);
+			this.cooldowns[2].renderCooldown = -30;
+		}
+    	this.cooldowns[2].setProgress(false);
+    }
+
+
+    public void drawToolTip(List stringList, int x, int y, FontRenderer font)
+    {
+    	if (!stringList.isEmpty())
+        {
+            GL11.glDisable(GL12.GL_RESCALE_NORMAL);
+            GL11.glDisable(GL11.GL_LIGHTING);
+            GL11.glDisable(GL11.GL_DEPTH_TEST);
+            int k = 0;
+            Iterator iterator = stringList.iterator();
+
+            while (iterator.hasNext())
+            {
+                String s = (String)iterator.next();
+                int l = font.getStringWidth(s);
+
+                if (l > k)
+                {
+                    k = l;
+                }
+            }
+
+            int j2 = x + 12;
+            int k2 = y - 12;
+            int i1 = 8;
+
+            if (stringList.size() > 1)
+            {
+                i1 += 2 + (stringList.size() - 1) * 10;
+            }
+
+            if (j2 + k > this.width)
+            {
+                j2 -= 28 + k;
+            }
+
+            if (k2 + i1 + 6 > this.height)
+            {
+                k2 = this.height - i1 - 6;
+            }
+
+            this.zLevel = 300.0F;
+            int j1 = -267386864;
+            this.drawGradientRect(j2 - 3, k2 - 4, j2 + k + 3, k2 - 3, j1, j1);
+            this.drawGradientRect(j2 - 3, k2 + i1 + 3, j2 + k + 3, k2 + i1 + 4, j1, j1);
+            this.drawGradientRect(j2 - 3, k2 - 3, j2 + k + 3, k2 + i1 + 3, j1, j1);
+            this.drawGradientRect(j2 - 4, k2 - 3, j2 - 3, k2 + i1 + 3, j1, j1);
+            this.drawGradientRect(j2 + k + 3, k2 - 3, j2 + k + 4, k2 + i1 + 3, j1, j1);
+            int k1 = 1347420415;
+            int l1 = (k1 & 16711422) >> 1 | k1 & -16777216;
+            this.drawGradientRect(j2 - 3, k2 - 3 + 1, j2 - 3 + 1, k2 + i1 + 3 - 1, k1, l1);
+            this.drawGradientRect(j2 + k + 2, k2 - 3 + 1, j2 + k + 3, k2 + i1 + 3 - 1, k1, l1);
+            this.drawGradientRect(j2 - 3, k2 - 3, j2 + k + 3, k2 - 3 + 1, k1, k1);
+            this.drawGradientRect(j2 - 3, k2 + i1 + 2, j2 + k + 3, k2 + i1 + 3, l1, l1);
+
+            for (int i2 = 0; i2 < stringList.size(); ++i2)
+            {
+                String s1 = (String)stringList.get(i2);
+                font.drawStringWithShadow(s1, j2, k2, -1);
+
+                if (i2 == 0)
+                {
+                    k2 += 2;
+                }
+
+                k2 += 10;
+            }
+
+            this.zLevel = 0.0F;
+            GL11.glEnable(GL11.GL_LIGHTING);
+            GL11.glEnable(GL11.GL_DEPTH_TEST);
+            GL11.glEnable(GL12.GL_RESCALE_NORMAL);
+        }
+    	
+    	
+    }
+    
+	private class ButtonState {
+
+		private boolean inProgress = false;
+		public int renderCooldown = 0;
+
+		public ButtonState(boolean inProgress, int renderCooldown) {
+			this.inProgress = inProgress;
+			this.renderCooldown = renderCooldown;
+		}
+
+		public void setProgress(boolean inProgress) {
+			this.inProgress = inProgress;
+		}
+
+		public boolean getProgress() {
+			return this.inProgress;
+		}
+
+	}
+
+}

990a91e552f941b28070cdfac275c7b5c2362244
PT400C <pt400c@gmail.com>
2019-03-29 20:42:26 +0100

    First release of 1.6.4

---
+++ src/main/java/de/pt400c/defaultsettings/CommandDefaultSettings.java
@@ -0,0 +1,146 @@
+package de.pt400c.defaultsettings;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.command.WrongUsageException;
+import net.minecraft.util.ChatMessageComponent;
+import net.minecraft.util.EnumChatFormatting;
+
+public class CommandDefaultSettings extends CommandBase {
+
+	public static final ArrayList<String> arg = new ArrayList<String>() {{	add("save");	}};
+	private ThreadPoolExecutor tpe = new ThreadPoolExecutor(1, 3, 10, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+	
+    @Override
+    public String getCommandName() {
+        return "defaultsettings";
+    }
+    
+    @Override
+    public List<String> getCommandAliases() {
+    	return new ArrayList<String>() {{	add("ds");	}};
+    }
+
+    @Override
+    public String getCommandUsage(ICommandSender sender) {
+        return "/defaultsettings [save]";
+    }
+
+    @Override
+    public int getRequiredPermissionLevel() {
+        return 0;
+    }
+    
+    @Override
+    public int compareTo(Object o) {
+    	return 0;
+    }
+
+    @Override
+    public void processCommand(final ICommandSender sender, String[] args) {
+    	if (args.length > 1 || !arg.contains(args[0].toLowerCase()))
+			throw new WrongUsageException(getCommandUsage(sender));
+
+		if (tpe.getQueue().size() > 0) {
+			sender.sendChatToPlayer(ChatMessageComponent.createFromText(EnumChatFormatting.RED + "Please wait until the last request has been processed!"));
+			return;
+		}
+
+		MutableBoolean issue = new MutableBoolean(false);
+
+		tpe.execute(new ThreadRunnable(sender, issue) {
+
+			@Override
+			public void run() {
+				try {
+					FileUtil.saveKeys();
+					sender.sendChatToPlayer(ChatMessageComponent.createFromText(EnumChatFormatting.GREEN + "Successfully saved the key configuration"));
+					FileUtil.restoreKeys();
+				} catch (Exception e) {
+					DefaultSettings.log.log(Level.SEVERE, "An exception occurred while saving the key configuration:", e);
+					sender.sendChatToPlayer(ChatMessageComponent.createFromText(EnumChatFormatting.RED + "Couldn't save the key configuration!"));
+					issue.setBoolean(true);
+				}
+			}
+		});
+
+		tpe.execute(new ThreadRunnable(sender, issue) {
+
+			@Override
+			public void run() {
+				try {
+					FileUtil.saveOptions();
+					sender.sendChatToPlayer(ChatMessageComponent.createFromText(EnumChatFormatting.GREEN + "Successfully saved the default game options"));
+				} catch (Exception e) {
+					DefaultSettings.log.log(Level.SEVERE, "An exception occurred while saving the default game options:", e);
+					sender.sendChatToPlayer(ChatMessageComponent.createFromText(EnumChatFormatting.RED + "Couldn't save the default game options!"));
+					issue.setBoolean(true);
+				}
+			}
+		});
+
+		tpe.execute(new ThreadRunnable(sender, issue) {
+			
+			@Override
+			public void run() {
+				try {
+					FileUtil.saveServers();
+					sender.sendChatToPlayer(ChatMessageComponent.createFromText(EnumChatFormatting.GREEN + "Successfully saved the server list"));
+				} catch (Exception e) {
+					DefaultSettings.log.log(Level.SEVERE, "An exception occurred while saving the server list:", e);
+					sender.sendChatToPlayer(ChatMessageComponent.createFromText(EnumChatFormatting.RED + "Couldn't save the server list!"));
+					issue.setBoolean(true);
+				}
+				
+				if (issue.getBoolean())
+					sender.sendChatToPlayer(ChatMessageComponent.createFromText(EnumChatFormatting.YELLOW + "Please inspect the log files for further information!"));
+			}
+		});
+
+	}
+
+    @Override
+    public List<String> addTabCompletionOptions(ICommandSender sender, String[] args) {
+        if(args.length < 2) {
+            return getListOfStringsMatchingLastWord(args, arg.toArray(new String[0]));
+        }
+		return new ArrayList<String>();
+    }
+    
+    abstract private class ThreadRunnable implements Runnable {
+ 	   
+        final ICommandSender supply;
+        final MutableBoolean issue;
+
+        ThreadRunnable(ICommandSender supply, MutableBoolean issue) {
+            this.supply = supply;
+            this.issue = issue;
+        }
+    }
+    
+    private class MutableBoolean {
+    	
+    	private boolean bool;
+    	
+    	public MutableBoolean(boolean bool) {
+			this.bool = bool;
+		}
+    	
+    	public boolean getBoolean() {
+    		return this.bool;
+    	}
+    	
+    	public void setBoolean(boolean bool) {
+    		this.bool = bool;
+    	}
+    	
+    }
+
+}
---
+++ src/main/java/de/pt400c/defaultsettings/DefaultSettings.java
@@ -0,0 +1,79 @@
+package de.pt400c.defaultsettings;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLConstructionEvent;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.TickRegistry;
+import cpw.mods.fml.relauncher.Side;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.common.MinecraftForge;
+
+@Mod(modid = DefaultSettings.MODID, name = DefaultSettings.NAME, version = DefaultSettings.VERSION, dependencies = "before:*", certificateFingerprint = "@FINGERPRINT@")
+public class DefaultSettings {
+
+	public static final String MODID = "defaultsettings";
+	public static final String NAME = "DefaultSettings";
+	public static final String VERSION = "@VERSION@";
+	public static final Logger log = LogManager.getLogManager().getLogger(DefaultSettings.MODID);
+	public static final boolean isServer = FMLCommonHandler.instance().getSide() == Side.SERVER;
+	public static boolean devEnv = Arrays.asList(MinecraftServer.class.getDeclaredFields()).get(7).getName().equals("hostname");
+	public static Map<String, Integer> keyRebinds = new HashMap<String, Integer>();
+	public static boolean setUp = false;
+
+	@Instance
+	public static DefaultSettings instance;
+
+	@EventHandler
+	public static void init(FMLInitializationEvent event) {
+		if (!isServer)
+			TickRegistry.registerTickHandler(new TickHandlerClient(), Side.CLIENT);
+	}
+
+	@EventHandler
+	public static void construction(FMLConstructionEvent event) {
+		if (isServer || setUp)
+			return;
+		try {
+			FileUtil.restoreContents();
+		} catch (Exception e) {
+			DefaultSettings.log.log(Level.SEVERE, "An exception occurred while starting up the game:", e);
+		}
+		setUp = true;
+	}
+
+	@EventHandler
+	public static void preInit(FMLPreInitializationEvent event) {
+		if (isServer)
+			return;
+		ClientCommandHandler.instance.registerCommand(new CommandDefaultSettings());
+		MinecraftForge.EVENT_BUS.register(DefaultSettings.class);
+	}
+
+	@EventHandler
+	public static void postInit(FMLPostInitializationEvent event) {
+		if (isServer)
+			return;
+
+		try {
+			FileUtil.restoreKeys();
+		} catch (IOException e) {
+			DefaultSettings.log.log(Level.SEVERE, "An exception occurred while starting up the game (Post):", e);
+		} catch (NullPointerException e) {
+			DefaultSettings.log.log(Level.SEVERE, "An exception occurred while starting up the game (Post):", e);
+		}
+	}
+
+}

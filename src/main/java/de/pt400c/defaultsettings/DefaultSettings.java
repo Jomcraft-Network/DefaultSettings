66f8a33982d3305666c88c947bdb69feb8dc8279
PT400C <pt400c@gmail.com>
2019-04-07 15:54:05 +0200

    Added KeyModifiers and fixed mcmod.info + removed unused code

--- src/main/java/de/pt400c/defaultsettings/DefaultSettings.java
+++ src/main/java/de/pt400c/defaultsettings/DefaultSettings.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import net.minecraft.launchwrapper.Launch;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.FMLCommonHandler;
@@ -17,38 +16,35 @@ import net.minecraftforge.fml.common.event.FMLConstructionEvent;
 import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = DefaultSettings.MODID, name = DefaultSettings.NAME, version = DefaultSettings.VERSION, dependencies = "before:*", certificateFingerprint = "@FINGERPRINT@")
+@Mod(modid = DefaultSettings.MODID, name = DefaultSettings.NAME, version = DefaultSettings.VERSION, dependencies = "before:*", certificateFingerprint = "@FINGERPRINT@", clientSideOnly = true)
 public class DefaultSettings {
 
 	public static final String MODID = "defaultsettings";
 	public static final String NAME = "DefaultSettings";
 	public static final String VERSION = "@VERSION@";
 	public static final Logger log = LogManager.getLogger(DefaultSettings.MODID);
-	public static final boolean isServer = FMLCommonHandler.instance().getSide() == Side.SERVER;
-	public static boolean devEnv = Launch.blackboard.get("fml.deobfuscatedEnvironment").equals(true);
-	public static Map<String, Integer> keyRebinds = new HashMap<String, Integer>();
-	public static boolean setUp = false;
+	public static Map<String, KeyContainer> keyRebinds = new HashMap<String, KeyContainer>();
 
 	@Instance
 	public static DefaultSettings instance;
+	
+	public DefaultSettings() {
+		instance = this;
+	}
 
 	@EventHandler
 	public static void construction(FMLConstructionEvent event) {
-		if (isServer || setUp)
-			return;
 		try {
 			FileUtil.restoreContents();
 		} catch (Exception e) {
 			DefaultSettings.log.log(Level.ERROR, "An exception occurred while starting up the game:", e);
 		}
-		setUp = true;
 	}
 	
 	@EventHandler
     public static void onFingerprintViolation(FMLFingerprintViolationEvent event) {
-		if(devEnv)
+		if(event.isDirectory())
 			return;
 		
 		DefaultSettings.log.log(Level.ERROR, "The mod's files have been manipulated! The game will be terminated.");
@@ -57,9 +53,6 @@ public class DefaultSettings {
 
 	@EventHandler
 	public static void preInit(FMLPreInitializationEvent event) {
-		if (isServer)
-			return;
-		
 		ClientCommandHandler.instance.registerCommand(new CommandDefaultSettings());
 		MinecraftForge.EVENT_BUS.register(DefaultSettings.class);
 		MinecraftForge.EVENT_BUS.register(new EventHandlers());
@@ -67,9 +60,6 @@ public class DefaultSettings {
 	
 	@EventHandler
 	public static void postInit(FMLPostInitializationEvent event) {
-		if (isServer)
-			return;
-
 		try {
 			FileUtil.restoreKeys();
 		} catch (IOException e) {
@@ -77,6 +67,10 @@ public class DefaultSettings {
 		} catch (NullPointerException e) {
 			DefaultSettings.log.log(Level.ERROR, "An exception occurred while starting up the game (Post):", e);
 		}
+	}
+	
+	public static DefaultSettings getInstance() {
+		return instance;
 	}
 
 }

89a63937899fbbb7a3336ec7108b563ad3570424
PT400C <pt400c@gmail.com>
2019-04-16 11:46:32 +0200

    Added new GUI + ASM Hook

---
+++ src/main/java/de/pt400c/defaultsettings/gui/Segment.java
@@ -0,0 +1,116 @@
+package de.pt400c.defaultsettings.gui;
+
+import static de.pt400c.defaultsettings.FileUtil.MC;
+
+import net.minecraft.client.audio.PositionedSoundRecord;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.WorldRenderer;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.util.ResourceLocation;
+
+public abstract class Segment {
+	
+	protected final GuiScreen gui;
+	
+	protected double posX;
+	protected double posY;
+	protected float width;
+	protected float height;
+	
+	public Segment(GuiScreen gui, float posX, float posY, float width, float height) {
+		this.gui = gui;
+		this.posX = posX;
+		this.posY = posY;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public abstract void render(float mouseX, float mouseY, float partialTicks);
+	
+	public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        return this.isSelected(mouseX, mouseY);
+	}
+	
+	public void hoverCheck(float mouseX, float mouseY) {}
+
+    public boolean mouseReleased(double mouseX, double mouseY, int button) {
+        return this.isSelected(mouseX, mouseY);
+    }
+    
+    public boolean mouseDragged(double mouseX, double mouseY, int button) {
+        return this.isSelected(mouseX, mouseY);
+    }
+	
+	public boolean isSelected(double mouseX, double mouseY) {
+        return mouseX >= this.getPosX() && mouseY >= this.getPosY() && mouseX < this.getPosX() + this.getWidth() && mouseY < this.getPosY() + this.getHeight();
+	}
+	
+	public double getPosX() {
+        return this.posX;
+	}
+	
+	public double getPosY() {
+        return this.posY;
+	}
+	
+	public float getWidth() {
+        return this.width;
+	}
+	
+	public float getHeight() {
+        return this.height;
+	}
+	
+	public Segment setPos(double x, double y) {
+		this.posX = x;
+		this.posY = y;
+		return this;
+	}
+	
+	public void clickSound() {
+        MC.getSoundHandler().playSound(PositionedSoundRecord.create(new ResourceLocation("gui.button.press"), 1.0F));
+    }
+	
+	public static void drawRect(double left, double top, double right, double bottom, int color)
+    {
+        if (left < right)
+        {
+        	double i = left;
+            left = right;
+            right = i;
+        }
+
+        if (top < bottom)
+        {
+        	double j = top;
+            top = bottom;
+            bottom = j;
+        }
+
+        float f3 = (float)(color >> 24 & 255) / 255.0F;
+        float f = (float)(color >> 16 & 255) / 255.0F;
+        float f1 = (float)(color >> 8 & 255) / 255.0F;
+        float f2 = (float)(color & 255) / 255.0F;
+        Tessellator tessellator = Tessellator.getInstance();
+        WorldRenderer worldrenderer = tessellator.getWorldRenderer();
+        GlStateManager.enableBlend();
+        GlStateManager.disableTexture2D();
+        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+        GlStateManager.color(f, f1, f2, f3);
+        worldrenderer.begin(7, DefaultVertexFormats.POSITION);
+        worldrenderer.pos(left, bottom, 0.0D).endVertex();
+        worldrenderer.pos(right, bottom, 0.0D).endVertex();
+        worldrenderer.pos(right, top, 0.0D).endVertex();
+        worldrenderer.pos(left, top, 0.0D).endVertex();
+        tessellator.draw();
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableBlend();
+    }
+	
+	public static void drawButton(double left, double top, double right, double bottom, int color, int color2, int border) {
+		drawRect(left, top, right, bottom, color);
+		drawRect(left + border, top + border, right - border, bottom - border, color2);
+	}
+}

8c381fe1b22e0686ad052db0c1ed3d2142274cc4
PT400C Jomcraft <pt400c@gmail.com>
2019-06-11 17:43:13 +0200

    Added DS config GUI to Forge config GUIs

--- src/main/java/de/pt400c/defaultsettings/DefaultSettings.java
+++ src/main/java/de/pt400c/defaultsettings/DefaultSettings.java
@@ -7,6 +7,8 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -40,6 +42,7 @@ public class DefaultSettings {
 		setUp = true;
 		MinecraftForge.EVENT_BUS.register(DefaultSettings.class);
 		MinecraftForge.EVENT_BUS.register(new EventHandlers());
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, ()-> (mc, screen) -> new GuiConfig(screen));
 		
 	}
 	
--- src/main/java/de/pt400c/defaultsettings/EventHandlers.java
+++ src/main/java/de/pt400c/defaultsettings/EventHandlers.java
@@ -1,14 +1,21 @@
 package de.pt400c.defaultsettings;
 
 import static org.lwjgl.glfw.GLFW.*;
+import java.lang.reflect.Field;
 import static de.pt400c.defaultsettings.FileUtil.MC;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.util.InputMappings;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.gui.GuiModList;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
 
+@Mod.EventBusSubscriber(modid = DefaultSettings.MODID, value = Dist.CLIENT)
 public class EventHandlers {
 
 	@SubscribeEvent
@@ -21,7 +28,28 @@ public class EventHandlers {
 	@SubscribeEvent
 	public void serverStarting(FMLServerStartingEvent event) {
 		CommandDefaultSettings.register(event);
-		
 	}
+	
+	@SubscribeEvent
+	public static void onGuiRender(GuiScreenEvent.DrawScreenEvent event) {
 
+		if (event.getGui() instanceof GuiModList) {
+			try {
+				GuiModList modList = (GuiModList) event.getGui();
+				Field modField = GuiModList.class.getDeclaredField("selectedMod");
+				modField.setAccessible(true);
+				ModInfo selectedMod = (ModInfo) modField.get(modList);
+				if (selectedMod != null && selectedMod.getModId().equals(DefaultSettings.MODID)) {
+					Field buttonField = GuiModList.class.getDeclaredField("configButton");
+					buttonField.setAccessible(true);
+					GuiButton button = (GuiButton) buttonField.get(modList);
+					button.enabled = true;
+				}
+				
+			} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+				//Forget about everything
+			}
+
+		}
+	}
 }

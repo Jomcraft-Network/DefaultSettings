466720eb4839097c93d025b7bd7cb9bdee1c61c1
PT400C Jomcraft <pt400c@gmail.com>
2019-07-22 20:17:53 +0200

    Updated "About" section

--- src/main/java/de/pt400c/defaultsettings/GuiConfig.java
+++ src/main/java/de/pt400c/defaultsettings/GuiConfig.java
@@ -11,6 +11,7 @@ import java.util.concurrent.TimeUnit;
 import org.apache.logging.log4j.Level;
 import org.lwjgl.input.Keyboard;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.MinecraftForge;
 import de.pt400c.defaultsettings.gui.ButtonMenuSegment;
 import de.pt400c.defaultsettings.gui.ButtonSegment;
 import de.pt400c.defaultsettings.gui.ButtonUpdateChecker;
@@ -97,9 +98,7 @@ public class GuiConfig extends DefaultSettingsGUI {
         						
         					addChild(new ScrollableSegment(this, 50, 30, width - 74 - 90, height - 25 - 10 - 30, (byte) 0))).addVariant(new MenuArea(this, 74, 25).
         			
-        					addChild(new ButtonSegment(this, 83, 56, "Useless", button -> {
-								return true;
-							}, 80, 25, 3))));
+        					addChild(new TextSegment(this, 25, 20, 20, 20, "DefaultSettings: " + MinecraftForge.MC_VERSION + "-" + DefaultSettings.VERSION + "\n\nCreated by Jomcraft Network, 2019", 0, false))));
     	
     	this.addSegment(new ButtonMenuSegment(0, this, 10, 34, "Save", button -> {
 			return true;

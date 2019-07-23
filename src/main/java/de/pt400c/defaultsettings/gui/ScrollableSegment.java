d61f5bc4ad5153c76b195a5de6c0e47212ac6cf2
PT400C Jomcraft <pt400c@gmail.com>
2019-07-23 13:48:40 +0200

    Added smooth scrolling to the GUI

--- src/main/java/de/pt400c/defaultsettings/gui/ScrollableSegment.java
+++ src/main/java/de/pt400c/defaultsettings/gui/ScrollableSegment.java
@@ -32,6 +32,7 @@ public class ScrollableSegment extends Segment {
 	private double distanceY = 0;
 	private int maxSize = 0;
 	private final byte id;
+	private float velocity = 0;
 
 	public ScrollableSegment(GuiScreen gui, float posX, float posY, int width, int height, byte id) {
 		super(gui, posX, posY, width, height, false);
@@ -99,28 +100,9 @@ public class ScrollableSegment extends Segment {
 	
 	@Override
 	public boolean handleMouseInput() {
-		int scroll = Mouse.getEventDWheel();
+		float scroll = Mouse.getEventDWheel();
 		this.maxSize = 18 + 20 * (this.list.size() - 1);
-		if (scroll != 0 && !this.grabbed) {
-
-			if (this.add + scroll / 60 > 0) {
-				this.add = -0;
-				return true;
-			}
-
-			if (scroll < 0) {
-				int yOff = this.maxSize + this.add + scroll / 60;
-				int movable = (int) (this.getPosY() + yOff);
-				int fix = (int) (this.getPosY() + height - 1);
-
-				if (movable < fix) {
-					return true;
-				}
-			}
-
-			this.add += scroll / 60;
-
-		}
+		this.velocity += scroll / 120F;
 		return true;
 	}
 	
@@ -132,6 +114,38 @@ public class ScrollableSegment extends Segment {
 	@Override
 	public void render(float mouseX, float mouseY, float partialTicks) {
 
+		this.add += (int) (this.velocity);
+		
+		if (this.add > 0) {
+			this.add = 0;
+			this.velocity = 0;
+		}
+
+		if (this.add < 0) {
+			int yOff = maxSize + this.add;
+			int movable = (int) (this.getPosY() + yOff);
+			int fix = (int) (this.getPosY() + height - 1);
+
+			if (movable < fix) {
+
+				int tempAdd = (int) (this.getPosY() + height - 1 - 20 * (this.list.size() - 1) - this.getPosY() - 18);
+				this.velocity = 0;
+				this.add = tempAdd;
+
+			}
+		}
+		
+		if(this.velocity > 0) {
+			this.velocity -= 1;
+			if(this.velocity - 1 == 0)
+				this.velocity -= 1;
+		}
+		else if(this.velocity < 0) {
+			this.velocity += 1;
+			if(this.velocity + 1 == 0)
+				this.velocity += 1;
+		}
+		
 		this.maxSize = 18 + 20 * (this.list.size() - 1);
 		int color = 0xff818181;
 

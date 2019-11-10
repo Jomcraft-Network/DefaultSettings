a8359a884d45378de7fbc6c059857eceb85866ab
PT400C Jomcraft <pt400c@gmail.com>
2019-11-10 10:25:14 +0100

    Fixed GL error

--- src/main/java/de/pt400c/defaultsettings/font/FontRendererClass.java
+++ src/main/java/de/pt400c/defaultsettings/font/FontRendererClass.java
@@ -495,7 +495,7 @@ public class FontRendererClass implements IResourceManagerReloadListener {
     	}
     	
     	MC.getTextureManager().bindTexture(bold ? bold_tex : tex);
-    	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     }
 

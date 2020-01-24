69107ebae07f45a0481411972324da6f68c8c074
PT400C Jomcraft <pt400c@gmail.com>
2020-01-24 10:11:38 +0000

    Update FileUtil.java

--- src/main/java/de/pt400c/defaultsettings/FileUtil.java
+++ src/main/java/de/pt400c/defaultsettings/FileUtil.java
@@ -56,7 +56,7 @@ public class FileUtil {
 	public static PrivateJSON privateJson;
 	public static final String privateLocation = "ds_private_storage.json";
 	public static final String mainLocation = "config/defaultsettings.json";
-	public static Thread registryChecker;
+	public volatile static Thread registryChecker;
 	public volatile static boolean options_exists = false;
 	public volatile static boolean keys_exists = false;
 	public volatile static boolean servers_exists = false;

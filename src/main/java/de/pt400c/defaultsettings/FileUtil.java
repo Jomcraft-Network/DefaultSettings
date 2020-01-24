871ec700f1061148cedcb7c9d0460e789d9e13d1
PT400C Jomcraft <pt400c@gmail.com>
2020-01-24 10:12:28 +0000

    Update FileUtil.java

--- src/main/java/de/pt400c/defaultsettings/FileUtil.java
+++ src/main/java/de/pt400c/defaultsettings/FileUtil.java
@@ -64,7 +64,7 @@ public class FileUtil {
 	public static final String privateLocation = "ds_private_storage.json";
 	public static final String mainLocation = "config/defaultsettings.json";
 	public static final boolean devEnv = (Boolean) Launch.blackboard.get("fml.deobfuscatedEnvironment");
-	public static Thread registryChecker;
+	public volatile static Thread registryChecker;
 	public volatile static boolean options_exists = false;
 	public volatile static boolean keys_exists = false;
 	public volatile static boolean servers_exists = false;

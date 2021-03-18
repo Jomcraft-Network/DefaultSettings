7264e046126e984d981b3e510d1b459e77908bba
PT400C Jomcraft <pt400c@gmail.com>
2021-03-18 16:02:24 +0100

    Revert "Fix namespace"
    This reverts commit 7c7f4c678dc321a05999600ee064e68490b4f4f1.

---
+++ src/main/java/de/pt400c/defaultsettings/PrivateJSON.java
@@ -0,0 +1,33 @@
+package de.pt400c.defaultsettings;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.apache.logging.log4j.Level;
+
+import net.jomcraft.neptunefx.NeptuneFX;
+
+public class PrivateJSON {
+	
+	public static transient final long serialVersionUID = 498123L;
+	public HashMap<String, String> currentHash = new HashMap<String, String>();
+	public String targetProfile = "!NEW!";
+	public String currentProfile = "!NEW!";
+	public String privateIdentifier = null;
+	private boolean framerateTransformAllowed = true;
+	public boolean compatibilityMode = false;
+	
+	public void save() {
+		try (FileWriter writer = new FileWriter(new File(NeptuneFX.mcDataDir, "ds_private_storage.json"))) {
+            NeptuneFX.gson.toJson(this, writer);
+        } catch (IOException e) {
+        	DefaultSettings.log.log(Level.ERROR, "Exception at processing startup: ", e);
+        }
+	}
+	
+	public boolean framerateASM() {
+		return this.framerateTransformAllowed;
+	}
+}
---
+++ src/main/java/de/pt400c/defaultsettings/gui/SplitterSegment.java
@@ -0,0 +1,85 @@
+package de.pt400c.defaultsettings.gui;
+
+import net.minecraft.client.gui.GuiScreen;
+import static net.jomcraft.neptunefx.NEX.*;
+import static org.lwjgl.opengl.GL14.glBlendFuncSeparate;
+import java.util.function.Function;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import de.pt400c.defaultsettings.GuiConfig;
+import static org.lwjgl.opengl.GL11.*;
+
+@SideOnly(Side.CLIENT)
+public class SplitterSegment extends BakedSegment {
+	
+	private final LeftMenu menu;
+	private final Function<GuiConfig, Integer> heightF;
+	
+	public SplitterSegment(GuiScreen gui, float posX, float posY, Function<GuiConfig, Integer> height, LeftMenu menu) {
+		super(gui, 0, posX, posY, 3F, height.apply((GuiConfig) gui), 255, 255, 255, true, false);
+		this.heightF = height;
+		this.menu = menu;
+	}
+
+	@Override
+	public void render(int mouseX, int mouseY, float partialTicks) {
+		
+		if(resized != this.resized_mark) 
+			height = heightF.apply((GuiConfig) this.gui);
+		
+		setup();
+
+		if(!compiled) {
+			preRender();
+	
+			glPushMatrix();
+
+			glDisable(GL_TEXTURE_2D);
+			glEnable(GL_BLEND);
+			glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ZERO);
+			glDisable(GL_ALPHA_TEST);
+			glShadeModel(GL_SMOOTH);
+			drawGradientCircle(0, 2.2F, 2F, 270, 75, 0xffaaaaaa, 0x00e6e6e6);
+			drawGradientCircle(0, this.getHeight() - 2.2F, 2F, 0, 75, 0xffaaaaaa, 0x00e6e6e6);
+			drawGradient(0, 2.2F, 2F, this.getHeight() - 2.2F, 0xffaaaaaa, 0x00e6e6e6, 0);
+			glShadeModel(GL_FLAT);
+
+			glEnable(GL_POINT_SMOOTH);
+
+			glPointSize(1.25F * (scaledresolution.getScaleFactor() / 2F));
+
+			glBegin(GL_POINTS);
+
+			glVertex3f(0.5F, 1, 0.0f);
+
+			glEnd();
+			glDisable(GL_POINT_SMOOTH);
+
+			glEnable(GL_POINT_SMOOTH);
+
+			glPointSize(1.25F * (scaledresolution.getScaleFactor() / 2F));
+
+			glBegin(GL_POINTS);
+
+			glVertex3f(0.5F, this.getHeight() - 1, 0.0f);
+
+			glEnd();
+			glDisable(GL_POINT_SMOOTH);
+
+			glDisable(GL_BLEND);
+			glEnable(GL_TEXTURE_2D);
+
+			drawRect(0, 1, 1, this.getHeight() - 1, 0xffe6e6e6, true, null, false);
+
+			glPopMatrix();
+
+			postRender(1, false);
+			
+		}
+		
+		glPushMatrix();
+		glTranslatef(-this.menu.offs, 0, 0);
+		drawTexture(1);
+		glPopMatrix();
+	}
+}

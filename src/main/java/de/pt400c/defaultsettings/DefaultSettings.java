dfdad91fe63e67f408e2e6ad44fcd68d40548d7f
PT400C Jomcraft <pt400c@gmail.com>
2021-03-18 15:25:10 +0100

    Revert "Init for NEX lib on 1.16"
    This reverts commit 7eabc475a90ab79520729dd57d5595195e47d243.

---
+++ src/main/java/de/pt400c/defaultsettings/DefaultSettings.java
@@ -0,0 +1,221 @@
+package de.pt400c.defaultsettings;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.jar.JarInputStream;
+import javax.net.ssl.HttpsURLConnection;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import com.electronwill.nightconfig.core.CommentedConfig;
+import com.electronwill.nightconfig.toml.TomlParser;
+import de.pt400c.defaultsettings.font.FontRendererClass;
+import net.minecraft.client.GameSettings;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLLoader;
+
+@Mod(value = DefaultSettings.MODID)
+public class DefaultSettings {
+
+	public static final String MODID = "defaultsettings";
+	public static final Logger log = LogManager.getLogger(DefaultSettings.MODID);
+	public static final String VERSION = getModVersion();
+	public static final String USER_AGENT = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2";
+	public static Map<String, KeyContainer> keyRebinds = new HashMap<String, KeyContainer>();
+	public static boolean setUp = false;
+	public static String BUILD_ID = "Unknown";
+	public static String BUILD_TIME = "Unknown";
+	public static FontRendererClass fontRenderer;
+	private static final UpdateContainer updateContainer = new UpdateContainer();
+	public static DefaultSettings instance;
+	public static final boolean debug = false;
+	public static boolean init = false;
+	public static Class<?> alphaTest;
+	public static int targetMS = 9;
+	public static boolean compatibilityMode = false;
+	
+	@SuppressWarnings({ "deprecation" })
+	public DefaultSettings() {
+		instance = this;
+
+		DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> {
+			if (setUp)
+				return;
+			
+			FMLJavaModLoadingContext.get().getModEventBus().addListener(this::postInit);
+			FMLJavaModLoadingContext.get().getModEventBus().addListener(this::regInit);
+
+			try {
+				FileUtil.restoreContents();
+				
+			} catch (Exception e) {
+				DefaultSettings.log.log(Level.ERROR, "An exception occurred while starting up the game:", e);
+			}
+			setUp = true;
+			MinecraftForge.EVENT_BUS.register(DefaultSettings.class);
+
+			MinecraftForge.EVENT_BUS.register(new EventHandlers());
+			MinecraftForge.EVENT_BUS.register(new UnregHandlers());
+
+			ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, () -> GuiConfig::new);
+			ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> "ANY", (remote, isServer) -> true));
+			
+			(new Thread() {
+
+				@Override
+				public void run() {
+					try {
+						sendCount();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					
+				}
+				
+			}).start();
+
+		});
+		
+		DistExecutor.runWhenOn(Dist.DEDICATED_SERVER, () -> () -> {
+			DefaultSettings.log.log(Level.WARN, "DefaultSettings is a client-side mod only! It won't do anything on servers!");
+		});
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static String getModVersion() {
+		//Stupid FG 3 workaround
+		TomlParser parser = new TomlParser();
+		InputStream stream = DefaultSettings.class.getClassLoader().getResourceAsStream("META-INF/mods.toml");
+		CommentedConfig file = parser.parse(stream);
+
+		return ((ArrayList<CommentedConfig>) file.get("mods")).get(0).get("version");
+	}
+	
+	/*
+    public void onFingerprintViolation(FMLFingerprintViolationEvent event) {
+		if(event.isDirectory() || FileUtil.isDev)
+			return;
+
+		DefaultSettings.log.log(Level.ERROR, "The mod's files have been manipulated! The game will be terminated.");
+		System.exit(0);
+    }*/
+
+	@SuppressWarnings("deprecation")
+	public void postInit(FMLLoadCompleteEvent event) {
+		DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> {
+			
+			fontRenderer = new FontRendererClass();
+			fontRenderer.readFontTexture();
+			
+			try {
+				getBuildID();
+				getBuildTime();
+			} catch (NullPointerException | IOException  e) {
+				DefaultSettings.log.log(Level.ERROR, "Something went wrong while starting up: ", e);
+			}
+			
+			try {
+				FileUtil.restoreKeys();
+			} catch (IOException e) {
+				DefaultSettings.log.log(Level.ERROR, "An exception occurred while starting up the game (Post):", e);
+			} catch (NullPointerException e) {
+				DefaultSettings.log.log(Level.ERROR, "An exception occurred while starting up the game (Post):", e);
+			}
+
+		});
+		
+		DistExecutor.runWhenOn(Dist.DEDICATED_SERVER, () -> () -> {
+			DefaultSettings.log.log(Level.WARN, "DefaultSettings is a client-side mod only! It won't do anything on servers!");
+		});
+
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void regInit(RegistryEvent.NewRegistry event) {
+		
+		if (!init) {
+
+			DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> {
+
+				try {
+
+					GameSettings gameSettings = FileUtil.MC.gameSettings;
+					gameSettings.loadOptions();
+					FileUtil.MC.gameSettings.saveOptions();
+
+				} catch (NullPointerException e) {
+					DefaultSettings.log.log(Level.ERROR, "Something went wrong while starting up: ", e);
+				}
+
+			});
+			DistExecutor.runWhenOn(Dist.DEDICATED_SERVER, () -> () -> {
+				DefaultSettings.log.log(Level.WARN, "DefaultSettings is a client-side mod only! It won't do anything on servers!");
+			});
+
+			init = true;
+
+		}
+	}
+	
+	private static void getBuildID() throws FileNotFoundException, IOException {
+		File file = FMLLoader.getLoadingModList().getModFileById(DefaultSettings.MODID).getFile().getFilePath().toFile();
+
+		try (JarInputStream jarStream = new JarInputStream(new FileInputStream(file))) {
+			BUILD_ID = jarStream.getManifest().getMainAttributes().getValue("Build-ID");
+		}
+	}
+	
+	private static void getBuildTime() throws FileNotFoundException, IOException {
+		File file = FMLLoader.getLoadingModList().getModFileById(DefaultSettings.MODID).getFile().getFilePath().toFile();
+
+		try (JarInputStream jarStream = new JarInputStream(new FileInputStream(file))) {
+			BUILD_TIME = jarStream.getManifest().getMainAttributes().getValue("Implementation-Timestamp");
+		}
+	}
+	
+	public static UpdateContainer getUpdater() {
+		return updateContainer;
+	}
+	
+	public static DefaultSettings getInstance() {
+		return instance;
+	}
+	
+	public static void sendCount() throws Exception {
+		String url = "https://apiv1.jomcraft.net/count";
+		String jsonString = "{\"id\":\"Defaultsettings\", \"code\":" + RandomStringUtils.random(32, true, true) + "}"; 
+		URL obj = new URL(url);
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(jsonString);
+
+		wr.flush();
+		wr.close();
+		con.getResponseCode();
+		con.disconnect();
+
+	}
+}
---
+++ src/main/java/de/pt400c/defaultsettings/gui/Segment.java
@@ -0,0 +1,117 @@
+package de.pt400c.defaultsettings.gui;
+
+import net.minecraft.client.audio.SimpleSound;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.util.SoundEvents;
+
+import static de.pt400c.defaultsettings.FileUtil.MC;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public abstract class Segment {
+	
+	protected final Screen gui;
+	protected float posX;
+	protected float posY;
+	protected float width;
+	protected float height;
+	protected float hitX;
+	protected float hitY;
+	protected final boolean isPopupSegment;
+	public static double scaledFactor;
+	public static int resized = 0;
+	protected int resized_mark = 0;
+	
+	public Segment(Screen gui, float posX, float posY, float width, float height, boolean popupSegment) {
+		this.gui = gui;
+		this.posX = posX;
+		this.posY = posY;
+		this.width = width;
+		this.height = height;
+		this.isPopupSegment = popupSegment;
+	}
+	
+	public void init() {};
+	
+	public abstract void render(int mouseX, int mouseY, float partialTicks);
+	
+	public void customRender(int mouseX, int mouseY, float customPosX, float customPosY, float partialTicks) {};
+	
+	public boolean mouseClicked(int mouseX, int mouseY, int button) {
+        return this.isSelected(mouseX, mouseY);
+	}
+	
+	public boolean mouseScrolled(float p_mouseScrolled_1_) {
+		int mouseX = (int) (MC.mouseHelper.getMouseX() * MC.mainWindow.getScaledWidth() / (double) MC.mainWindow.getWidth());
+        int mouseY = (int) (MC.mouseHelper.getMouseY() * MC.mainWindow.getScaledHeight() / (double) MC.mainWindow.getHeight());
+        return this.isSelected(mouseX, mouseY);
+    }
+    
+	public boolean charTyped(char p_charTyped_1_, int p_charTyped_2_) {
+    	return false;
+    }
+	
+	public boolean keyPressed(int p_keyPressed_1_, int p_keyPressed_2_, int p_keyPressed_3_) {
+    	return false;
+    }
+    
+    public void guiContentUpdate(String... arg) {};
+	
+	public boolean hoverCheck(int mouseX, int mouseY) { return false; }
+
+    public boolean mouseReleased(int mouseX, int mouseY, int button) {
+        return this.isSelected(mouseX, mouseY);
+    }
+    
+    public boolean mouseDragged(int mouseX, int mouseY, int button) {
+        return this.isSelected(mouseX, mouseY);
+    }
+	
+	public boolean isSelected(int mouseX, int mouseY) {
+		return (((DefaultSettingsGUI) this.gui).popupField == null || this.getIsPopupSegment()) && mouseX >= this.getPosX() + this.hitX && mouseY >= this.getPosY() + this.hitY && mouseX < this.getPosX() + this.hitX + this.getWidth() && mouseY < this.getPosY() + this.hitY + this.getHeight();
+	}
+	
+	public float getPosX() {
+        return this.posX;
+	}
+	
+	public float getPosY() {
+        return this.posY;
+	}
+	
+	public boolean getIsPopupSegment() {
+        return this.isPopupSegment;
+	}
+	
+	public float getWidth() {
+        return this.width;
+	}
+	
+	public float getHeight() {
+        return this.height;
+	}
+	
+	public void initSegment() {
+
+	}
+	
+	public Segment setPos(float x, float y) {
+		this.posX = x;
+		this.posY = y;
+		return this;
+	}
+	
+
+	public Segment setPosHit(float x, float y, float x2, float y2) {
+		this.posX = x;
+		this.posY = y;
+		this.hitX = x2;
+		this.hitY = y2;
+		return this;
+	}
+	
+	public void clickSound() {
+		MC.getSoundHandler().play(SimpleSound.master(SoundEvents.UI_BUTTON_CLICK, 1.0F));
+    }
+}

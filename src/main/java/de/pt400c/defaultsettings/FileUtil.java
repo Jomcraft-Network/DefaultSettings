2ce0600306bd3622592ee068d825784339df1aee
PT400C Jomcraft <pt400c@gmail.com>
2021-03-28 09:30:20 +0200

    Version polish

--- src/main/java/de/pt400c/defaultsettings/FileUtil.java
+++ src/main/java/de/pt400c/defaultsettings/FileUtil.java
@@ -340,7 +340,8 @@ public class FileUtil {
 			mainJson.save();
 		}else {
 
-			copyAndHashPrivate();
+			if(otherCreator)
+				copyAndHashPrivate();
 			mainJson.setExportMode(false);
 			mainJson.save();
 			

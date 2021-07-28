922713169bcc7562e5ad662cac30d7b89dac03df
PT400C Jomcraft <pt400c@gmail.com>
2021-07-28 13:23:18 +0200

    Try to implement resourcepack handling

--- src/main/java/net/jomcraft/defaultsettings/DefaultSettings.java
+++ src/main/java/net/jomcraft/defaultsettings/DefaultSettings.java
@@ -1,16 +1,12 @@
 package net.jomcraft.defaultsettings;
 
 import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.jar.JarInputStream;
 import javax.net.ssl.HttpsURLConnection;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.Level;
@@ -18,18 +14,17 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import com.electronwill.nightconfig.core.CommentedConfig;
 import com.electronwill.nightconfig.toml.TomlParser;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.Options;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.loading.FMLLoader;
 
 @Mod(value = DefaultSettings.MODID)
 public class DefaultSettings {
@@ -40,8 +35,6 @@ public class DefaultSettings {
 	public static final String USER_AGENT = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2";
 	public static Map<String, KeyContainer> keyRebinds = new HashMap<String, KeyContainer>();
 	public static boolean setUp = false;
-	public static String BUILD_ID = "Unknown";
-	public static String BUILD_TIME = "Unknown";
 	private static final UpdateContainer updateContainer = new UpdateContainer();
 	public static DefaultSettings instance;
 	public static boolean init = false;
@@ -105,13 +98,6 @@ public class DefaultSettings {
 		DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> {
 
 			try {
-				getBuildID();
-				getBuildTime();
-			} catch (NullPointerException | IOException e) {
-				DefaultSettings.log.log(Level.ERROR, "Something went wrong while starting up: ", e);
-			}
-
-			try {
 				FileUtil.restoreKeys(true, FileUtil.firstBootUp);
 			} catch (IOException e) {
 				DefaultSettings.log.log(Level.ERROR, "An exception occurred while starting up the game (Post):", e);
@@ -127,48 +113,24 @@ public class DefaultSettings {
 
 	}
 
-	@SuppressWarnings("deprecation")
-	public void regInit(FMLLoadCompleteEvent event) {
+	@SuppressWarnings({ "deprecation", "resource" })
+	public void regInit(RegistryEvent.NewRegistry event) {
 		if (!init) {
 			DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> {
-				event.enqueueWork(new Runnable() {
+				try {
+					Options gameSettings = Minecraft.getInstance().options;
+					gameSettings.load();
+					Minecraft.getInstance().options.save();
 
-					@SuppressWarnings("resource")
-					@Override
-					public void run() {
-						try {
-							Options gameSettings = Minecraft.getInstance().options;
-							gameSettings.load();
-							Minecraft.getInstance().options.save();
+				} catch (NullPointerException e) {
+					DefaultSettings.log.log(Level.ERROR, "Something went wrong while starting up: ", e);
+				}
 
-						} catch (NullPointerException e) {
-							DefaultSettings.log.log(Level.ERROR, "Something went wrong while starting up: ", e);
-						}
-
-					}
-
-				});
 			});
 			DistExecutor.runWhenOn(Dist.DEDICATED_SERVER, () -> () -> {
 				DefaultSettings.log.log(Level.WARN, "DefaultSettings is a client-side mod only! It won't do anything on servers!");
 			});
 			init = true;
-		}
-	}
-
-	private static void getBuildID() throws FileNotFoundException, IOException {
-		File file = FMLLoader.getLoadingModList().getModFileById(DefaultSettings.MODID).getFile().getFilePath().toFile();
-
-		try (JarInputStream jarStream = new JarInputStream(new FileInputStream(file))) {
-			BUILD_ID = jarStream.getManifest().getMainAttributes().getValue("Build-ID");
-		}
-	}
-
-	private static void getBuildTime() throws FileNotFoundException, IOException {
-		File file = FMLLoader.getLoadingModList().getModFileById(DefaultSettings.MODID).getFile().getFilePath().toFile();
-
-		try (JarInputStream jarStream = new JarInputStream(new FileInputStream(file))) {
-			BUILD_TIME = jarStream.getManifest().getMainAttributes().getValue("Implementation-Timestamp");
 		}
 	}
 

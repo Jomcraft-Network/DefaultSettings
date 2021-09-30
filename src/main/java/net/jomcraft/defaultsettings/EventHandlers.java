626aecc1b6ffd53ed51329d0ad82797d9c8a52ce
PT400C Jomcraft <pt400c@gmail.com>
2021-09-30 09:19:03 +0200

    Optifine and settings fixes

--- src/main/java/net/jomcraft/defaultsettings/EventHandlers.java
+++ src/main/java/net/jomcraft/defaultsettings/EventHandlers.java
@@ -4,7 +4,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
 
 public class EventHandlers {
-	
+
 	@SubscribeEvent
 	public void serverStarting(FMLServerStartingEvent event) {
 		CommandDefaultSettings.register(event);
--- src/main/java/net/jomcraft/defaultsettings/KeyContainer.java
+++ src/main/java/net/jomcraft/defaultsettings/KeyContainer.java
@@ -4,13 +4,13 @@ import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraftforge.client.settings.KeyModifier;
 
 public class KeyContainer {
-	
+
 	public final InputConstants.Key input;
 	public final KeyModifier modifier;
-	
+
 	public KeyContainer(final InputConstants.Key input, final KeyModifier modifier) {
 		this.input = input;
 		this.modifier = modifier;
 	}
-	
+
 }

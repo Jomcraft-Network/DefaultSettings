ef8f8304827ca4ce4e720389292064aad1326ac8
PT400C Jomcraft <pt400c@gmail.com>
2022-03-17 23:29:20 +0100

    Fix for wonky Forge class renaming habits

---
+++ src/main/java/net/jomcraft/defaultsettings/EventNew.java
@@ -0,0 +1,34 @@
+package net.jomcraft.defaultsettings;
+
+import org.apache.logging.log4j.Level;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.Options;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.registries.NewRegistryEvent;
+
+public class EventNew {
+
+	@SuppressWarnings({ "deprecation", "resource" })
+	public void regInitNew(NewRegistryEvent event) {
+		if (!DefaultSettings.init) {
+			DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> {
+				try {
+					Options gameSettings = Minecraft.getInstance().options;
+					gameSettings.load();
+					Minecraft.getInstance().options.save();
+
+				} catch (NullPointerException e) {
+					DefaultSettings.log.log(Level.ERROR, "Something went wrong while starting up: ", e);
+				}
+
+			});
+			DistExecutor.runWhenOn(Dist.DEDICATED_SERVER, () -> () -> {
+				DefaultSettings.log.log(Level.WARN, "DefaultSettings is a client-side mod only! It won't do anything on servers!");
+			});
+			DefaultSettings.init = true;
+		}
+	}
+
+}

b86c14208c6146c688411995ccf524c4c0aa2348
PT400C Jomcraft <pt400c@gmail.com>
2025-10-02 09:41:12 +0200

    Hack Minecraft to not save prematurely

--- src/main/java/net/jomcraft/defaultsettings/FabricCoreHook.java
+++ src/main/java/net/jomcraft/defaultsettings/FabricCoreHook.java
@@ -180,4 +180,9 @@ public class FabricCoreHook implements ICoreHook {
     public void restoreKeys(boolean update, boolean initial) throws IOException {
         FileUtil.restoreKeys(update, initial);
     }
+
+    @Override
+    public void saveOptionsFile() {
+        Minecraft.getInstance().options.save();
+    }
 }

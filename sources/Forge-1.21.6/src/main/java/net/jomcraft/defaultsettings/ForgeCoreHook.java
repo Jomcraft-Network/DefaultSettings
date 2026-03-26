a864063406266d56a4104275c830fb23d1cf8e87
PT400C Jomcraft <pt400c@gmail.com>
2026-03-26 18:26:12 +0100

    Fix broken keys restore mechanism on 1.21.5
    - Removed unnecessary mixin
    - Fix forcefully overwriting keybinds

--- sources/Forge-1.21.6/src/main/java/net/jomcraft/defaultsettings/ForgeCoreHook.java
+++ sources/Forge-1.21.6/src/main/java/net/jomcraft/defaultsettings/ForgeCoreHook.java
@@ -84,7 +84,7 @@ public class ForgeCoreHook implements ICoreHook {
                 mappings[i].defaultKey = container.input;
 
                 ObfuscationReflectionHelper.setPrivateValue(KeyMapping.class, mappings[i], container.modifier, "keyModifierDefault");
-                mappings[i].setKeyModifierAndCode(mappings[i].getDefaultKeyModifier(), container.input);
+                //mappings[i].setKeyModifierAndCode(mappings[i].getDefaultKeyModifier(), container.input);
                 break;
             }
         }
